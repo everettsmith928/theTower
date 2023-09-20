7c189108cc3b846afcf95e2865938264ca3067ee
everettsmith928 <36865028+everettsmith928@users.noreply.github.com>
2023-09-19 21:37:09 -0600

    env

---
+++ theTower.client/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-6yliz80lpaeg4tpp.us.auth0.com'
+export const clientId = 'AqGHl2IaH0w2MxZvdoM0aOey8yPgfwlx'
+export const audience = 'https://testapi.com'
