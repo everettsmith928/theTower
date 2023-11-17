488997ca7ac159f6104f3e2a4321574f528bb242
everettsmith928 <everett.marsland.smith@gmail.com>
2023-11-17 14:27:50 -0700

    push for deploy

--- client/env.js
+++ client/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : '/tower'
 export const useSockets = false
 export const domain = 'dev-6yliz80lpaeg4tpp.us.auth0.com'
 export const clientId = 'AqGHl2IaH0w2MxZvdoM0aOey8yPgfwlx'
